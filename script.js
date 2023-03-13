c5944e54d67859d17bd4611891aef99f859ef5e2
arasuramanan <arasuaudi@gmail.com>
2023-03-13 13:53:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+var counter = document.getElementById("count");
+let time = 10;
+setTimeout(() => {
+time--
+counter.innerText = time;
+
+setTimeout(() => {
+    time--
+    counter.innerText = time;
+    
+    setTimeout(() => {
+        time--
+        counter.innerText = time;
+
+        setTimeout(() => {
+            time--
+            counter.innerText = time;
+
+            setTimeout(() => {
+                time--
+                counter.innerText = time;
+
+                setTimeout(() => {
+                    time--
+                    counter.innerText = time;
+
+                    setTimeout(() => {
+                        time--
+                        counter.innerText = time;
+
+                        setTimeout(() => {
+                            time--
+                            counter.innerText = time;
+
+                            setTimeout(() => {
+                                time--
+                                counter.innerText = time;
+
+
+                                    setTimeout(() => {
+                                        time--
+                                        counter.innerText = "Happy Independence Day";
+    
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
